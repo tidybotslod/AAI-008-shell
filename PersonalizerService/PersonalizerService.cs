fddedd7f96ff580615d2fb92e9d26f202a6fecfb
tidybots <tidybotslod@outlook.com>
2021-03-18 10:44:35 -0700

    Create shell for Lab AAI-008

---
+++ PersonalizerService/PersonalizerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Personalizer;
+using Microsoft.Azure.CognitiveServices.Personalizer.Models;
+
+namespace AAI
+{
+    public partial class PersonalizerService
+    {
+
+    }
+}
